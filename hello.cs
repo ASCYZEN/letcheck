e6a4f20a9bfb46832b2da1e6c8257e25ae96e928
ASCYZEN <86395865+ASCYZEN@users.noreply.github.com>
2021-06-24 17:21:49 +0530

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("push to github");
+            Console.WriteLine("lets try again");
